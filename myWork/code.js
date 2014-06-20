f2eb111af2b5c4aa0748cfefd9e04e121a51ceda
Partha Sarma <ambition.logic@gmail.com>
2014-06-20 17:20:30 +0530

    added external js file

---
+++ myWork/code.js
@@ -0,0 +1,13 @@
+var scoop = 5;
+
+while(scoop>0){
+	document.write("Another scoop!!<br>");
+		if (scoop >= 5) {
+			alert("Eat faster!! the icecream is about to melt");
+		}else if (scoop < 3) {
+			alert("icecream is running low");
+		}
+	scoop--;
+}
+
+document.write("Life without icecream is not the Same!! :-(");
